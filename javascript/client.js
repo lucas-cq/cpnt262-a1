ce3c55ddcb67a1aa6c47f2180267933687edc3f0
Lucas Cummings <lucas.cummings024@gmail.com>
2021-10-14 18:06:34 -0600

    Added button, This time it works

--- javascript/client.js
+++ javascript/client.js
@@ -1,29 +1,11 @@
-const hamburgerMenu = document.querySelector('.hamburger');
-const hamburgerClose = document.querySelector('.close-menu')
+// Initial Variables
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
 
-
-const openMenu = function() {
-  const nav = document.querySelector('.mobile-nav');
-  nav.style.display = "block";
-  nav.style.textAlign = "center";
-  hamburgerMenu.classList.toggle('mobile-nav');
-  const barIcon = document.querySelector('.bar');
-  barIcon.style.display = "none";
-  const crossIcon = document.querySelector('.cross');
-  crossIcon.style.display = "flex";
+// Open/Close funtion on Navigation
+const openNav = function () {
+  hamburger.classList.toggle('active');
+  mobileNav.classList.toggle('active');
 }
 
-hamburgerMenu.addEventListener('click', openMenu)
-
-const closeMenu = function() {
-  const nav = document.querySelector('.mobile-nav');
-  nav.style.width = "0";
-  nav.style.display = "none";
-  hamburgerClose.classList.toggle('close-menu');
-  const barIcon = document.querySelector('.bar');
-  barIcon.style.display = "flex";
-  const crossIcon = document.querySelector('.cross');
-  crossIcon.style.display = "none";
-}
-
-hamburgerClose.addEventListener('click', closeMenu)
+hamburger.addEventListener('click', openNav);
